10e1b8f97f63173fb119660c411bc8fce1e793af
Rehman Murad Ali <rehman.muradali@venturedive.com>
2019-01-01 13:26:13 +0500

    Making War File for Cross Platform

--- src/main/java/com/rehman/timeline/TimelineApplication.java
+++ src/main/java/com/rehman/timeline/TimelineApplication.java
@@ -1,22 +1,21 @@
 package com.rehman.timeline;
 
-import com.rehman.timeline.model.Event;
-import com.rehman.timeline.services.EventService;
-import com.rehman.timeline.util.StringUtil;
-import com.rehman.timeline.util.Utility;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TimelineApplication {
+public class TimelineApplication extends SpringBootServletInitializer {
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(TimelineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TimelineApplication.class, args);
+    }
 
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 }
